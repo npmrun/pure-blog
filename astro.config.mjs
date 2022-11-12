fa2fbaacc50425d74cceb78edfffe2f12044f53c
npmrun <62639956+npmrun@users.noreply.github.com>
2022-11-12 09:49:30 +0800

    3 files modified, 2 added, 1 deleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,24 @@ import { defineConfig } from 'astro/config';
 import path from 'path';
 import remarkBlock from './plugins/remark-block.mjs';
 import Directive from 'remark-directive';
- 
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   markdown: {
-    syntaxHighlight: "prism",
+    syntaxHighlight: 'prism',
     extendDefaultPlugins: true,
     remarkPlugins: [Directive, [remarkBlock, {}]],
   },
   integrations: [],
   vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: ` @import "@blog/styles/_global.scss"; `,
+        },
+      },
+    },
     ssr: {
       external: ['svgo'],
     },
