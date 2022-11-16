02e072279d402eecb55e5433a9229c0dc636053b
npmrun <kk1549469775@gmail.com>
2022-11-16 15:40:59 +0800

    规范化路径

--- src/share/index.ts
+++ src/share/index.ts
@@ -1,5 +1,5 @@
 import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
+import { normalizePath } from 'vite'
 
 // 最好不要用下面这种方式获取cwd,开发时与打包时不同
 // 开发时是根据目录的
@@ -8,5 +8,5 @@ import { fileURLToPath } from 'url';
 // export const cwd = resolve(__dirname, '../../');
 
 export const cwd = process.cwd(); // 路径符号不同的操作系统可能不同
-export const articleDir = resolve(cwd, './article').replace(/\\/g, "/");
-export const articleRoute = resolve(cwd, './src/pages').replace(/\\/g, "/");
+export const articleDir = normalizePath(resolve(cwd, './article'));
+export const articleRoute = normalizePath(resolve(cwd, './src/pages'));
